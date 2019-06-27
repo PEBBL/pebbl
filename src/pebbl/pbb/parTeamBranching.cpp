ba99e9a2d08b2d61640bf1ea17590f0adb5eb216
Will Loughlin <william.loughlin@stonybrook.edu>
2019-06-27 11:42:26 -0700

    First commit of parTeamBranching classes

---
+++ src/pebbl/pbb/parTeamBranching.cpp
@@ -0,0 +1,55 @@
+// parTeamBranching.cpp
+//
+// Parallel search and computation class code for PEBBL.
+//
+// Will Loughlin and Rohith Rokkam
+
+#include <pebbl_config.h>
+
+#include <pebbl/bb/branching.h>
+#include <pebbl/pbb/parBranching.h>
+#include <pebbl/pbb/teamBranching.h>
+#include <pebbl/pbb/parTeamBranching.h>
+#include <pebbl/utilib/exception_mngr.h>
+
+
+
+#include <sys/types.h>
+#include <unistd.h>
+
+
+#ifdef ACRO_HAVE_MPI
+
+#include <pebbl/comm/mpiComm.h>
+
+
+using namespace std;
+
+namespace pebbl {
+
+  // not sure how to deal with this. Might have to make it static to deal with the need 
+  // to set the seach communicator on instantiation
+void parallelTeamBranching::splitCommunicator(mpiComm worldComm, int teamSize, int clusterSize, int hubsDontWorkSize){
+  return;
+}
+
+double parallelTeamBranching::search(){
+  setTeam(teamComm);
+  if(iAmHead()){
+    return parallelSearchFramework(NULL);
+  }
+  else {
+    awaitWork();
+  }
+  return 0;
+}
+
+void parallelTeamBranching::rampUpSearch(){
+  setTeam(worldComm);
+  parallelBranching::rampUpSearch();
+}
+
+bool parallelTeamBranching::setup(int& argc, char**& argv, mpiComm worldComm = MPI_WORLDCOMM){
+  return true;
+}
+
---
+++ src/pebbl/pbb/parTeamBranching.h
@@ -0,0 +1,47 @@
+// parTeamBranching.cpp
+//
+// Parallel search and computation class code for PEBBL.
+//
+// Will Loughlin and Rohith Rokkam
+
+
+
+ 
+#ifndef pebbl_parTeamBranching_h
+#define pebbl_parTeamBranching_h
+
+
+#ifdef ACRO_HAVE_MPI
+
+namespace pebbl {
+
+// Class that will implement methods used by the application for
+// parallel search with computation
+  class parallelTeamBranching : public virtual teamBranching,
+                                public virtual parallelBranching
+  {
+    protected:
+      
+      // Communicator including all processors
+      mpiComm worldComm;
+
+      // Communicator to hold the teamComm during operations like ramp up
+      mpiComm boundComm; 
+
+    public:
+      
+      // Splits a world comm into team comms and a search comm according to the parameters passed to pebbl
+      void splitCommunicator(mpiComm worldComm, int teamSize, int clusterSize, int hubsDontWorkSize);
+
+      // Overrides the search function of parBranching
+      virtual double search(); 
+
+      // Override ramp up search to use the global communicator during ramp up
+      void rampUpSearch();
+
+      bool setup(int& argc, char**& argv, mpiComm worldComm = MPI_WORLDCOMM);
+  }
+}
+
+#endif
+
